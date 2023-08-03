d94920e4790a57c8d7578d2d5fced085d98887f5
Ümmügülsüm Erol <92275196+ummugulsumm@users.noreply.github.com>
2023-08-03 17:56:42 +0300

    Add files via upload

--- Countries/country.js
+++ Countries/country.js
@@ -1,10 +1,70 @@
 const countryName = new URLSearchParams(location.search).get('name')
+const flag = document.querySelector(".country-details img")
+const countryNameElement = document.querySelector(".country-details h1")
+const nativeName = document.querySelector("#nativeName")
+const population = document.querySelector("#population")
+const region = document.querySelector("#region")
+const subRegion = document.querySelector("#subRegion")
+const capital = document.querySelector("#capital")
+const topLevelDomain = document.querySelector("#topLevelDomain")
+const currencies = document.querySelector("#currencies")
+const languages = document.querySelector("#languages")
+const borderCountries = document.querySelector(".border-countries")
+
 
 fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
     .then((result) => result.json())
     .then((data) => {
-        data.forEach((country) => {
-            console.log(country)
-    })
+             country = data[0]
+             console.log(country)
+            flag.src = country.flags.svg
+            flag.alt = `${country.name.common} Flag`
+            countryNameElement.innerHTML = country.name.common
+            population.innerHTML = country.population.toLocaleString()
+            region.innerHTML = country.region
+            
+            
+            topLevelDomain.innerHTML = country.tld.join(", ")
+
+            if(country.subregion) {
+                subRegion.innerHTML = country.subregion
+            }
+            
+            if(country.capital) {
+                capital.innerHTML = country.capital?.[0]
+            }
+
+            if(country.name.nativeName) {
+                nativeName.innerHTML = Object.values(country.name.nativeName)[0].common
+            } else {
+                nativeName.innerHTML = country.name.common
+            }
+
+            if(country.currencies) {
+                currencies.innerHTML = Object.values(country.currencies).map((currency) => currency.name).join(", ")
+            }
+
+            if(country.languages) {
+                languages.innerHTML = Object.values(country.languages).join(", ")
+            }
+
+
+            if(country.borders) {
+                country.borders.forEach((border) => {
+                    fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+                        .then((result) => result.json())
+                        .then(([borderCountry]) => {
+                            const borderCountryTag = document.createElement("a")
+                            borderCountryTag.href = `./country.html?name=${borderCountry.name.common}`
+                            borderCountryTag.innerText = borderCountry.name.common
+                            borderCountries.append(borderCountryTag)
+                        })
+                    
+                })
+            }
+
+
+
+
 })
 
--- Countries/script.js
+++ Countries/script.js
@@ -8,16 +8,16 @@ fetch("https://restcountries.com/v3.1/all")
             console.log(country)
             const countryCard = document.createElement('a')
             countryCard.classList.add("country-card")
-            countryCard.href = `/country.html?name=${country.name.common}`
+            countryCard.href = `./country.html?name=${country.name.common}`
             countryCard.innerHTML = `
                 <div class="flag">
-                    <img src="${country.flags.svg}" alt="Flag Image">
+                    <img src=" ${country.flags.svg}" alt="Flag Image">
                 </div>
                 <div class="card-text">
-                    <h3 class="card-title">${country.name.common}</h3>
-                    <p><b>Population: </b>${(country.population).toLocaleString()}</p>
-                    <p><b>Region: </b>${country.region}</p>
-                    <p><b>Capital: </b>${country.capital?.[0]}</p>
+                    <h3 class="card-title"> ${country.name.common}</h3>
+                    <p><b>Population: </b> ${(country.population).toLocaleString()}</p>
+                    <p><b>Region: </b> ${country.region}</p>
+                    <p><b>Capital: </b> ${country.capital?.[0]}</p>
                 </div>
             `
             countriesContainer.append(countryCard)
