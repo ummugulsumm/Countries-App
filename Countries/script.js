11024ab7b39c805000b09e5534abc98216425f5a
Ümmügülsüm Erol <92275196+ummugulsumm@users.noreply.github.com>
2023-08-04 17:53:12 +0300

    Add files via upload

--- Countries/script.js
+++ Countries/script.js
@@ -1,9 +1,23 @@
 
 const countriesContainer = document.querySelector(".countries-container")
 
+const filterByRegion = document.querySelector(".filter-by-region")
+
 fetch("https://restcountries.com/v3.1/all")
     .then((result) => result.json())
-    .then((data) => {
+    .then(renderCountries)
+
+
+filterByRegion.addEventListener('change', (e) => {
+        console.log(e.target.value);
+        fetch(`https://restcountries.com/v3.1/region/${e.target.value}`)
+            .then((result) => result.json())
+            .then(renderCountries)
+})
+
+    
+function renderCountries(data) {  
+        countriesContainer.innerHTML = ""
         data.forEach((country) => {
             console.log(country)
             const countryCard = document.createElement('a')
@@ -17,11 +31,12 @@ fetch("https://restcountries.com/v3.1/all")
                     <h3 class="card-title"> ${country.name.common}</h3>
                     <p><b>Population: </b> ${(country.population).toLocaleString()}</p>
                     <p><b>Region: </b> ${country.region}</p>
-                    <p><b>Capital: </b> ${country.capital?.[0]}</p>
+                    <p><b>Capital: </b> ${country.capital?.[0] || ""}</p>
                 </div>
             `
             countriesContainer.append(countryCard)
 
         })
-    })
 
+    
+}
